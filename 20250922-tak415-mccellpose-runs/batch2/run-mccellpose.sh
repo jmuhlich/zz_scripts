d660cb1e528df34cc0baab966c4b9dc4fd6e0c17
Jeremy Muhlich <jmuhlich@bitflood.org>
2025-10-13 22:16:53 -0400

    Add batch2

---
+++ 20250922-tak415-mccellpose-runs/batch2/run-mccellpose.sh
@@ -0,0 +1,25 @@
+echo ===========
+echo LSP18304
+/home/jmuhlich/miniconda3/envs/work/bin/mprof run -o mprofile_LSP18304.dat mccellpose -i /n/files/HiTS/lsp-analysis/cycif-production/110-BRCA-Mutant-Ovarian-Precursors/ORION-FISH-72525_Tanjina/October2025-Batch2/LSP18304/registration/LSP18304.ome.tif --output-cell LSP18304-cellpose-cell.ome.tif --output-nucleus LSP18304-cellpose-nucleus.ome.tif -c 1 --expand-size 2 --use-gpu --jobs 2
+echo ===========
+echo ------
+echo ===========
+echo LSP18316
+/home/jmuhlich/miniconda3/envs/work/bin/mprof run -o mprofile_LSP18316.dat mccellpose -i /n/files/HiTS/lsp-analysis/cycif-production/110-BRCA-Mutant-Ovarian-Precursors/ORION-FISH-72525_Tanjina/October2025-Batch2/LSP18316/registration/LSP18316.ome.tif --output-cell LSP18316-cellpose-cell.ome.tif --output-nucleus LSP18316-cellpose-nucleus.ome.tif -c 1 --expand-size 2 --use-gpu --jobs 2
+echo ===========
+echo ------
+echo ===========
+echo LSP19422
+/home/jmuhlich/miniconda3/envs/work/bin/mprof run -o mprofile_LSP19422.dat mccellpose -i /n/files/HiTS/lsp-analysis/cycif-production/110-BRCA-Mutant-Ovarian-Precursors/ORION-FISH-72525_Tanjina/October2025-Batch2/LSP19422/registration/LSP19422.ome.tif --output-cell LSP19422-cellpose-cell.ome.tif --output-nucleus LSP19422-cellpose-nucleus.ome.tif -c 1 --expand-size 2 --use-gpu --jobs 2
+echo ===========
+echo ------
+echo ===========
+echo LSP35817_P110
+/home/jmuhlich/miniconda3/envs/work/bin/mprof run -o mprofile_LSP35817_P110.dat mccellpose -i /n/files/HiTS/lsp-analysis/cycif-production/110-BRCA-Mutant-Ovarian-Precursors/ORION-FISH-72525_Tanjina/October2025-Batch2/LSP35817_P110/registration/LSP35817_P110.ome.tif --output-cell LSP35817_P110-cellpose-cell.ome.tif --output-nucleus LSP35817_P110-cellpose-nucleus.ome.tif -c 1 --expand-size 2 --use-gpu --jobs 2
+echo ===========
+echo ------
+echo ===========
+echo LSP35823_P110
+/home/jmuhlich/miniconda3/envs/work/bin/mprof run -o mprofile_LSP35823_P110.dat mccellpose -i /n/files/HiTS/lsp-analysis/cycif-production/110-BRCA-Mutant-Ovarian-Precursors/ORION-FISH-72525_Tanjina/October2025-Batch2/LSP35823_P110/registration/LSP35823_P110.ome.tif --output-cell LSP35823_P110-cellpose-cell.ome.tif --output-nucleus LSP35823_P110-cellpose-nucleus.ome.tif -c 1 --expand-size 2 --use-gpu --jobs 2
+echo ===========
+echo ------
