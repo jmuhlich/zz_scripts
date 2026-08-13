49a1218b69cc48ffadff3f7cf188dd8458f91a21
Jeremy Muhlich <jmuhlich@bitflood.org>
2026-08-13 12:25:52 -0400

    Improve efficiency, now usable (but slow) on large images

--- emo963-edof-p228e3.py
+++ emo963-edof-p228e3.py
@@ -15,7 +15,6 @@ import dask.array as da
 import numpy as np
 import skimage
 import tifffile
-import zarr
 
 ## Make a temp zarr copy of just the 3 DNA channels we need.
 #path_in = '/n/files/HiTS/lsp-analysis/cycif-production/228-joshi-mouse-tls/p228e3_mouse_TLS_reconstruction/registration/LSP70623.ome.tif'
@@ -24,46 +23,40 @@ import zarr
 #img = img[[0, 5, 10]]
 #da.to_zarr(img, 'LSP70623-dna123.zarr')
 
-# img = da.from_zarr('LSP70623-dna123.zarr')
+img = da.from_zarr('LSP70623-dna123.zarr')
 
-img = da.from_zarr(tifffile.imread('LSP70623-focus-test-crop.ome.tif', level=0, aszarr=True))
+#img = img[:, 2000:12000, 20000:30000]
 
-img_laplacian = da.stack([
-    da.map_overlap(skimage.filters.laplace, imgc, depth=1, boundary=0).astype(np.float32)
-    for imgc in img
-])
-img_laplacian = da.rechunk(img_laplacian, (1, 100, 100))
-
-focus_score = da.var(
-    da.lib.stride_tricks.sliding_window_view(img_laplacian, (30, 30), axis=(1, 2)),
-    axis=(-2, -1),
-)
-focus_labels = da.argmax(focus_score, axis=0).astype(np.uint8)
-focus_labels = da.rechunk(focus_labels, 1024)
-footprint = skimage.morphology.disk(10)
-focus_labels_f = da.map_overlap(skimage.filters.rank.median, focus_labels, depth=12, boundary=0)
-focus_labels_f = da.rechunk(da.pad(focus_labels_f, [[15, 14], [15, 14]]), 1024)
-
-def onehot(labels, num_labels, block_info=None):
-    res = np.zeros((num_labels,) + labels.shape, np.float32)
-    np.put_along_axis(res, labels[None], values=1, axis=0)
+def calc_edof(img):
+    img_norm = img / np.quantile(img, 0.9, axis=(1, 2))[:, None, None]
+    img_laplacian = da.stack([
+        skimage.filters.laplace(imgc).astype(np.float32) for imgc in img_norm
+    ])
+    img_laplacian = da.rechunk(img_laplacian, (1, 60, 60))
+    focus_score = da.var(
+        da.lib.stride_tricks.sliding_window_view(img_laplacian, (30, 30), axis=(1, 2)),
+        axis=(-2, -1),
+    )
+    focus_labels = np.argmax(focus_score, axis=0).astype(np.uint8).compute()
+    footprint = skimage.morphology.disk(10)
+    focus_labels_f = skimage.filters.rank.median(focus_labels, footprint=footprint)
+    focus_labels_f = np.pad(focus_labels_f, [[15, 14], [15, 14]])
+    weights = np.zeros(img.shape, np.float32)
+    np.put_along_axis(weights, focus_labels_f[None], values=1, axis=0)
+    for wc in weights:
+        cv2.GaussianBlur(wc, (0, 0), 2.5, wc)
+    res = np.round(np.sum(img * weights, axis=0)).astype(np.uint16)
     return res
 
-weights = da.map_blocks(
-    onehot,
-    focus_labels_f,
-    len(img),
-    chunks=(len(img),) + focus_labels_f.chunks,
+img_out = da.map_overlap(
+    calc_edof,
+    img.rechunk((img.shape[0],) + img.chunksize[1:]),
+    depth=(0, 36, 36),
+    boundary=0,
+    drop_axis=0,
+    dtype=np.uint16,
 )
-weights = da.rechunk(weights, (1,) + weights.chunks[1:])
-
-def blur(a):
-    return cv2.GaussianBlur(a[0], (0, 0), 2.5)[None]
-
-weights_f = da.map_overlap(blur, weights, depth=(0, 10, 10), boundary=0)
-
-img_out = da.round(da.sum(img * weights_f, axis=0)).astype(np.uint16)
-
+store = da.to_zarr(img_out, 'LSP70623-edof.zarr', mode='w', compute=False)
 with dask.diagnostics.ProgressBar():
-    da.to_zarr(img_out, 'LSP70623-edof.zarr', mode='w')
+    store.compute(scheduler='synchronous')
 print('Wrote output image to: LSP70623-edof.zarr')
